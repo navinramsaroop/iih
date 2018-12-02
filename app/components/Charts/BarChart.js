ee34e242a96b1b3dbef48d467bed757b993862cc
Connor Reinhold <cyr7@cornell.edu>
2018-12-02 14:33:46 -0500

    heifhefe

--- app/components/Charts/BarChart.js
+++ app/components/Charts/BarChart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {View, Text, StyleSheet, TouchableOpacity, Linking} from 'react-native'
 import {BarChart, Grid, YAxis} from 'react-native-svg-charts'
+import {_mailFunc} from '../../mailUtil/mailUtil.js'
 
 const data = [1,2,3,4,5,13,2,3,4,5,1,1,2,2,23,1,7]
 const contentInset = { top: 20, bottom: 20 }
@@ -12,10 +13,17 @@ export default class Bar extends React.Component{
     }
     render(){
         return (
-            <TouchableOpacity>
+            <TouchableOpacity onPress = {()=> {
+                this._exportScreen()}
+            }>
                 <Text> Hello </Text>
             </TouchableOpacity>
         
         )
     }
+    async _exportScreen() {
+        _mailFunc(
+          ['navinr13@gmail.com']
+        );
+      }
 }
